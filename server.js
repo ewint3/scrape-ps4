78bff3c1f2bc8b80e4f48b5b92f22b7db18d12f4
Weezer <ewint3@gmail.com>
2019-11-26 19:00:25 -0600

    setting up require in server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const express    = require('express'),
+      exphbs     = require('express-handlebars'),
+      bodyParser = require('body-parser'),
+      mongoose   = require('mongoose'),
+      cheerio    = require('cheerio'),
+      
